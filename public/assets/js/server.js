9777afceb750d842e5ac27555f7015a16b7c19b2
Blaze Harmon <concentratedcreatives@gmail.com>
2023-12-27 19:04:10 -0500

    Updated Server.js

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -5,43 +5,27 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }), express.json(), express.static('public'));
 
-// Routes
-const sendFile = (res, fileName) =>
-  res.sendFile(path.join(__dirname, `../../${fileName}.html`));
+const sendFile = (res, fileName) => res.sendFile(path.join(__dirname, `../../${fileName}.html`));
+
+const handleApiError = (res, error) => (console.error('Error reading or parsing db.json:', error), res.status(500).send('Internal Server Error'));
 
 app.get('/notes', (req, res) => sendFile(res, 'notes'));
 app.get('*', (req, res) => sendFile(res, 'index'));
 
-// API Routes
-app.get('/notes', (req, res) => {
-    try {
-      const notesData = fs.readFileSync('db/db.json', 'utf8');
-      const notes = notesData ? JSON.parse(notesData) : [];
-      res.json(notes);
-    } catch (error) {
-      console.error('Error reading or parsing db.json:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
-  
-  app.post('/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
+  try { res.json(JSON.parse(fs.readFileSync('db/db.json', 'utf8')) || []); } 
+  catch (error) { handleApiError(res, error); }
+});
+
+app.post('/api/notes', (req, res) => {
+  try {
     const { body } = req;
-    const notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
-  
-    body.id = generateUniqueId();
-    notes.push(body);
-  
+    const notes = [...JSON.parse(fs.readFileSync('db/db.json', 'utf8')), { ...body, id: Math.random().toString(36).substr(2, 9) }];
     fs.writeFileSync('db/db.json', JSON.stringify(notes));
     res.json(body);
-  });
-  
-
-function generateUniqueId() {
-  return Math.random().toString(36).substr(2, 9);
-}
+  } catch (error) { handleApiError(res, error); }
+});
 
 app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
